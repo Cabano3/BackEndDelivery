c3b0d7e8bba3419008b7cd8a4b92136802b0bced
Cristian Baño <cristian1898abr@outlook.es>
2020-08-05 16:59:25 -0500

    BackEnd

---
+++ SlnDeliveryBack/BEUDelivery/Transaction/UsuarioBLL.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BEUDelivery.Transaction
+{
+	public class UsuarioBLL
+	{
+		public static Usuario Get(int? id)
+		{
+			DeliveryEntities db = new DeliveryEntities();
+			return db.Usuario.Find(id);
+		}
+
+		public static void Create(Usuario u)
+		{
+			using (DeliveryEntities db = new DeliveryEntities())
+			{
+				using (var transaction = db.Database.BeginTransaction())
+				{
+					try
+					{
+						db.Usuario.Add(u);
+						db.SaveChanges();
+						transaction.Commit();
+					}
+					catch (Exception)
+					{
+						transaction.Rollback();
+						throw;
+					}
+				}
+			}
+		}
+
+		public static void Update(Usuario usuario)
+		{
+			using (DeliveryEntities db = new DeliveryEntities())
+			{
+				using (var transaction = db.Database.BeginTransaction())
+				{
+					try
+					{
+						db.Usuario.Attach(usuario);
+						db.Entry(usuario).State = System.Data.Entity.EntityState.Modified;
+						db.SaveChanges();
+						transaction.Commit();
+					}
+					catch (Exception ex)
+					{
+						transaction.Rollback();
+						throw ex;
+					}
+				}
+			}
+		}
+
+		public static void Delete(int? id)
+		{
+			using (DeliveryEntities db = new DeliveryEntities())
+			{
+				using (var transaction = db.Database.BeginTransaction())
+				{
+					try
+					{
+						Usuario usuario = db.Usuario.Find(id);
+						db.Entry(usuario).State = System.Data.Entity.EntityState.Deleted;
+						db.SaveChanges();
+						transaction.Commit();
+					}
+					catch (Exception ex)
+					{
+						transaction.Rollback();
+						throw ex;
+					}
+				}
+			}
+		}
+
+		public static List<Usuario> List()
+		{
+			DeliveryEntities db = new DeliveryEntities();
+			return db.Usuario.ToList();
+		}
+	}
+}
